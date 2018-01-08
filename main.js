4a2ae184efd230c515f5fe9a65089e6a7ba27e96
login12 <peter-sd@hotmail.com>
2018-01-08 21:38:19 -0200

    Indentation

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Create database for things.
 var path = require('path');
 var Datastore = require('nedb');
-var db = new Datastore({ filename: path.join(process.cwd(), 'data.db'), autoload: true });
+var db = new Datastore({filename: path.join(process.cwd(), 'data.db'), autoload: true});
 
 // Set variables and load modules
 var cryptom = require('crypto');
@@ -11,8 +11,8 @@ var socket = new JsonSocket(new net.Socket());
 var nacl = require(path.join(process.cwd(), 'src/js/nacl.js'));
 
 // Get BrowserWindow.
-const { remote } = require('electron');
-const { BrowserWindow } = remote;
+const {remote} = require('electron');
+const {BrowserWindow} = remote;
 
 var RaiWallet = require('rai-wallet');
 var Wallet = RaiWallet.Wallet;
@@ -35,36 +35,36 @@ socket.on('error', function() {
 // On RaiLightServer sucess connection:
 socket.on('connect', function() {
 	// Get first BlockCount ;)
-    socket.sendMessage({requestType: "getBlocksCount"});
+	socket.sendMessage({requestType: "getBlocksCount"});
 
-//	socket.sendMessage({requestType: "getBalance", address: "xrb_39ymww61tksoddjh1e43mprw5r8uu1318it9z3agm7e6f96kg4ndqg9tuds4"});
-//	socket.sendMessage({requestType: "getInfo", address: "xrb_39ymww61tksoddjh1e43mprw5r8uu1318it9z3agm7e6f96kg4ndqg9tuds4"});
+	//	socket.sendMessage({requestType: "getBalance", address: "xrb_39ymww61tksoddjh1e43mprw5r8uu1318it9z3agm7e6f96kg4ndqg9tuds4"});
+	//	socket.sendMessage({requestType: "getInfo", address: "xrb_39ymww61tksoddjh1e43mprw5r8uu1318it9z3agm7e6f96kg4ndqg9tuds4"});
 
 	// Handle RaiLightServer responses
-    socket.on('message', function(r) {
-	// If BlocksCount
-	if (r.type == "BlocksCount") {
-		// Update on frontend
-		$("#block").html("Block: "+r.count);
-	} else {
-		// Debug, for now.
-		console.log(r);
-	}
+	socket.on('message', function(r) {
+		// If BlocksCount
+		if (r.type == "BlocksCount") {
+			// Update on frontend
+			$("#block").html("Block: "+r.count);
+		} else {
+			// Debug, for now.
+			console.log(r);
+		}
     });
 });
 
-db.find({ type: 'wallet' }, function (err, docs) {
+db.find({type: 'wallet'}, function (err, docs) {
 	if(docs && docs.length){
-		$( document ).ready(function() {
-			$( "#wallet1" ).removeClass('selected');
-			$( "#wallet2" ).addClass('selected');
-			$("#content").load( "pages/index.pg" );
+		$(document).ready(function() {
+			$("#wallet1").removeClass('selected');
+			$("#wallet2").addClass('selected');
+			$("#content").load("pages/index.pg");
 		});
 	} else {
-		$( document ).ready(function() {
-			$( "#wallet1" ).addClass('selected');
-			$( "#wallet2" ).removeClass('selected');
-			$("#content").load( "pages/create.pg");
+		$(document).ready(function() {
+			$("#wallet1").addClass('selected');
+			$("#wallet2").removeClass('selected');
+			$("#content").load("pages/create.pg");
 		});		
 	}
 });
@@ -107,22 +107,22 @@ $("#minbtn").click(function() {
 // FUNCTIONS
 
 function encrypt(text, password){
-  var cipher = cryptom.createCipher('aes-256-cbc',password);
-  var crypted = cipher.update(text,'utf8','hex');
-  crypted += cipher.final('hex');
-  return crypted;
+	var cipher = cryptom.createCipher('aes-256-cbc',password);
+	var crypted = cipher.update(text,'utf8','hex');
+	crypted += cipher.final('hex');
+	return crypted;
 }
 
 function decrypt(text, password){
-  var decipher = cryptom.createDecipher('aes-256-cbc',password);
-  var dec = decipher.update(text,'hex','utf8');
-  dec += decipher.final('utf8');
-  return dec;
+	var decipher = cryptom.createDecipher('aes-256-cbc',password);
+	var dec = decipher.update(text,'hex','utf8');
+	dec += decipher.final('utf8');
+	return dec;
 }
 
 // Dev stupid things, for testing.		
 $("#submit").submit(function(e) {
-    e.preventDefault();
-    var test = $("#test").val();
+	e.preventDefault();
+	var test = $("#test").val();
 	socket.sendMessage({msg: test});
 });
